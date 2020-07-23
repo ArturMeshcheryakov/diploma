757501a08dca6f1889d2bb461bb32306f8786487
Artur Meshcheryakov <arthur.188@yandex.ru>
2020-07-23 22:50:51 +0300

    4 commit

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,6 +4,16 @@ const modal = () => {
   const popup = document.querySelectorAll('.popup');
   const btnModals = document.querySelectorAll('.js-modal');
 
+  const animationModal = (target) => {
+    const formWrapper = target.querySelector('.form-wrapper');
+    const animation = formWrapper.animate([
+      {opacity: '0'},
+      {opacity: '1'}
+    ], 600);
+
+    animation.addEventListener('finish', () => formWrapper.style.opacity = '1');
+  };
+
   btnModals.forEach((item) => {
     item.addEventListener('click', (event) => {
       let dataPopup = item.getAttribute('data-popup');
@@ -15,13 +25,15 @@ const modal = () => {
         if (popupItem.id === dataPopup) {
           popupItem.style.display = 'block';
 
+          animationModal(popupItem);
+
           popupItem.addEventListener('click', (event) => {
             const target = event.target;
             const formContentModal = target.closest('.form-content');
             const closeIconModal = target.closest('.close_icon');
             const closeBtnModal = target.closest('.close-btn');
 
-            if (popupItem.id === 'gift') document.querySelector('.fixed-gift').classList.remove('hide');
+            if (!formContentModal || closeIconModal || closeBtnModal) if (popupItem.id === 'gift') document.querySelector('.fixed-gift').classList.remove('hide');
             if (!formContentModal || closeIconModal) popupItem.style.display = 'none';
             if (closeBtnModal) {
               scrollElem(event, closeBtnModal);
