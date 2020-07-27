fdbd1c0ecf9a75e79d72c80fc3f304dba2b0deac
Artur Meshcheryakov <arthur.188@yandex.ru>
2020-07-27 21:27:14 +0300

    9 commit

--- src/modules/scrollTop.js
+++ src/modules/scrollTop.js
@@ -14,8 +14,11 @@ const scrollTop = () => {
     const scrollStep = window.pageYOffset / 40;
 
     if (window.pageYOffset > 0) {
+      document.body.classList.add('over_hide');
       window.scrollBy(0, -(scrollStep));
       setTimeout(backToTop, 0);
+    } else {
+      document.body.classList.remove('over_hide');
     }
   };
 
