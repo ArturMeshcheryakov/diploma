f53e83c160be89caa28700e1efd1d70856b81fae
Artur Meshcheryakov <arthur.188@yandex.ru>
2020-07-27 16:12:46 +0300

    8 commit

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,6 +1,7 @@
 const slider = () => {
   const gallerySliders = document.querySelector('.gallery-slider.js-slider');
   const mainSlider = document.querySelector('.main-slider.js-slider');
+  const servicesSlider = document.querySelector('.services-slider.js-slider');
 
   const carousel = (slider, time) => {
     if (!slider) return;
@@ -104,8 +105,76 @@ const slider = () => {
     }
   };
 
+  const tapeCarousel = (slider) => {
+    if (!slider) return;
+
+    const tape = slider.querySelector('.js-slider-carousel');
+    const slide = slider.querySelectorAll('.slide');
+    let showElem = 5;
+    const margin = +getComputedStyle(slide[0]).marginRight.replace('px', '') * 2;
+    const padding = +getComputedStyle(slider).paddingRight.replace('px', '') * 2;
+    let width = slide[0].offsetWidth + margin;
+    let position = 0;
+
+    const response = () => {
+      const width = window.screen.width;
+
+      if (width >= 1200) showElem = 5;
+      if (width < 1200) showElem = 4;
+      if (width < 992) showElem = 3;
+      if (width < 768) showElem = 2;
+      if (width < 481) showElem = 1;
+    };
+
+    window.addEventListener('resize', () => {
+      response();
+      responseCarousel(showElem);
+    });
+
+    const responseCarousel = (showElem) => {
+      slider.style.width = (width * showElem) + padding + 'px';
+    };
+
+    const next = () => {
+      if (-position === slide.length * width - (showElem * width)) {
+        position = 0;
+      } else {
+        position -= width;
+      }
+
+      tape.style.transform = `translateX(${position}px)`;
+    };
+
+    const prev = () => {
+      if (position === 0) {
+        position = (slide.length * width - (showElem * width)) * (-1);
+      } else {
+        position += width;
+      }
+
+      tape.style.transform = `translateX(${position}px)`;
+    };
+
+    slider.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const target = event.target;
+      const slideNext = target.closest('.slider-next');
+      const slidePrev = target.closest('.slider-prev');
+
+      if (!slideNext && !slidePrev) return;
+      if (slideNext) next();
+      if (slidePrev) prev();
+
+    });
+
+    response();
+    responseCarousel(showElem);
+  };
+
   carousel(gallerySliders, 2000);
   carousel(mainSlider, 3000);
+  tapeCarousel(servicesSlider);
 };
 
 export default slider;
